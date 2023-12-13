4f35ea3b3744174bffd348833d0753b4fbdb43c8
Milan Jobanputra <107670121+Milan-Jobanputra@users.noreply.github.com>
2023-12-13 18:12:21 +0530

    Add files via upload

---
+++ Features.js
@@ -0,0 +1,13 @@
+// Features.js
+import React from 'react';
+
+const Features = () => {
+  return (
+    <section>
+      <h2>Key Features</h2>
+      {/* Add features list here */}
+    </section>
+  );
+};
+
+export default Features;
