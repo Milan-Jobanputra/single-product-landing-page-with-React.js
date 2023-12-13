2795d0ed64ae10e46ebbbeab436e189cf56258ef
Milan Jobanputra <107670121+Milan-Jobanputra@users.noreply.github.com>
2023-12-13 18:11:58 +0530

    Add files via upload

---
+++ ProductDetails.js
@@ -0,0 +1,13 @@
+// ProductDetails.js
+import React from 'react';
+
+const ProductDetails = () => {
+  return (
+    <section>
+      <h2>Product Details</h2>
+      {/* Add product information here */}
+    </section>
+  );
+};
+
+export default ProductDetails;
