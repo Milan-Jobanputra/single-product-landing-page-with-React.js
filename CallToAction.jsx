380d9c79bbce7054d8847e25086fc2c9c677be38
Milan Jobanputra <107670121+Milan-Jobanputra@users.noreply.github.com>
2023-12-14 18:40:10 +0530

    Update CallToAction.jsx

--- CallToAction.jsx
+++ CallToAction.jsx
@@ -5,7 +5,11 @@ import React from 'react';
 const CallToAction = () => {
   return (
    
-
+      <section>
+      <h2>Ready to get started?</h2>
+      <p>Learn more about our amazing product and make a purchase now!</p>
+      {/* Add your call-to-action button here */}
+      </section>
     
   );
 };
